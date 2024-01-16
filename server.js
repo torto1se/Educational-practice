4130ada504c17a260aa22de37a1ee628b9c6bd48
torto1se <akvvsjcvdhi@gmail.com>
2024-01-16 20:24:47 +0300

    update

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const PORT = 3000;
+const morgan = require('morgan');
+
 app.set('view engine', 'ejs');
 
 const createPath = (page) => path.resolve(__dirname, 'ejs-views', `${page}.ejs`);
@@ -12,13 +14,9 @@ app.listen(PORT, (error) =>{
     error ? console.log(error) : console.log(`listening port ${PORT}`);
 });
 
-app.use((req, res, next) => {
-    console.log(`path: ${req.path}`);
-    console.log(`method: ${req.method}`);
-    next();
-});
-
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms'));
 app.use(express.static('styles'));
+app.use(express.urlencoded({extended: false}));
 
 app.get('/', (req, res) => {
     const title = 'Home';
@@ -28,7 +26,7 @@ app.get('/', (req, res) => {
 app.get('/contacts', (req, res) => {
     const title = 'Contacts';
     const contacts = [
-        { name: 'YouTube', link: 'http://youtube.com/YauhenKavalchuk' },
+        { name: 'YouTube', link: 'http://youtube.com/@torto1se' },
         { name: 'Twitter', link: 'http://github.com/YauhenKavalchuk' },
         { name: 'GitHub', link: 'http://twitter.com/YauhenKavalchuk' },
     ]
@@ -41,12 +39,40 @@ app.get('/about-us', (req, res) => {
 
 app.get('/posts/:id', (req, res) => {
     const title = 'Post';
-    res.render(createPath('post'), {title});
+    const post = {
+        id: '1', 
+        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente quidem provident, dolores, vero laboriosam nemo mollitia impedit unde fugit sint eveniet, minima odio ipsum sed recusandae aut iste aspernatur dolorem.',
+        title: 'Post title',
+        date: '05.05.2021',
+        author: 'Yauhen',
+    };
+    res.render(createPath('post'), {title, post});
 });
 
 app.get('/posts', (req, res) => {
     const title = 'Post';
-    res.render(createPath('posts'), {title});
+    const posts = [
+        {
+            id: '1', 
+            text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente quidem provident, dolores, vero laboriosam nemo mollitia impedit unde fugit sint eveniet, minima odio ipsum sed recusandae aut iste aspernatur dolorem.',
+            title: 'Post title',
+            date: '05.05.2021',
+            author: 'Yauhen',
+        }
+    ];
+    res.render(createPath('posts'), {title, posts});
+});
+
+app.post('/add-post', (req, res) => {
+    const { title, author, text} = req.body;
+    const post = {
+        id: new Date(),
+        date: (new Date()).toLocaleDateString(),
+        title,
+        author,
+        text,
+    }
+    res.render(createPath('post'), {post, title});
 });
 
 app.get('/add-post', (req, res) => {
